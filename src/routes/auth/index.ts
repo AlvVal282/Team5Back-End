10893a1641aacd10a23d8581a0e240eefc3b4d45
AlvVal282 <alvarovd@outlook.com>
2024-11-07 14:16:25 -0800

    Big PUSH

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -2,12 +2,9 @@ import express, { Router } from 'express';
 
 import { signinRouter } from './login';
 import { registerRouter } from './register';
-import { deleteAccountRouter} from './deleteAccount';
-import { forgottenPasswordRouter } from './forgottenPassword';
-import { resetPasswordRouter } from './resetPassword';
 
 const authRoutes: Router = express.Router();
 
-authRoutes.use(signinRouter, forgottenPasswordRouter, registerRouter, deleteAccountRouter, resetPasswordRouter);
+authRoutes.use(signinRouter, registerRouter);
 
 export { authRoutes };

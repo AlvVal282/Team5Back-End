05be5fa5783015b468fee3db1aa66eede686b98e
labanlab <labanlab@uw.edu>
2024-11-08 11:41:50 -0800

    forgotten and reset password + some apicdoc improvements

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -4,13 +4,18 @@ import { signinRouter } from './login';
 import { registerRouter } from './register';
 import { deleteAccountRouter} from './deleteAccount';
 import { checkToken } from '../../core/middleware';
-// import { forgottenPasswordRouter } from './forgottenPassword';
-// import { resetPasswordRouter } from './resetPassword';
+import { forgottenPasswordRouter } from './forgottenPassword';
+import { resetPasswordRouter } from './resetPassword';
 
 const authRoutes: Router = express.Router();
 
 authRoutes.use(signinRouter);
 authRoutes.use(registerRouter);
 authRoutes.use('/delete', checkToken, deleteAccountRouter);
+authRoutes.use(forgottenPasswordRouter);
+authRoutes.use(checkToken, resetPasswordRouter);
 
 export { authRoutes };
+
+
+

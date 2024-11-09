38919f999e923e5426f7614499b92f0f21425605
Austin Maggert <maggertaustin@gmail.com>
2024-11-08 19:53:39 -0800

    for real this time

--- src/routes/books/delete/index.ts
+++ src/routes/books/delete/index.ts
@@ -2,13 +2,11 @@ import express, { Router } from 'express';
 import { deleteAuthorRouter } from './deleteAuthor';
 import { deleteBookRouter } from './deleteISBN';
 import { deleteYearRouter } from './deleteYear';
-import { checkToken } from '../../../core/middleware';
 
 const deleteRoutes: Router = express.Router();
 
-deleteRoutes.use(checkToken, deleteBookRouter);
-deleteRoutes.use(checkToken, deleteAuthorRouter);
-deleteRoutes.use(checkToken, deleteYearRouter);
+deleteRoutes.use(deleteBookRouter);
+deleteRoutes.use(deleteAuthorRouter);
+deleteRoutes.use(deleteYearRouter);
 
 export { deleteRoutes };
-
